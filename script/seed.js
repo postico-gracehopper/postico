cd6d6c0bbafc1b1da5f3d6a19f07ce645f6aa3a8
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-10 13:50:51 -0600

    Basic user seed successful

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,71 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User },
+  // ASK JP: WHAT IS THE SKI MODEL CALLED? PRODUCT OR SOMETHING ELSE?
+  // WHAT OTHER MODELS SHALL WE SEED?
+} = require("../server/db");
+const { faker } = require("@faker-js/faker");
 
-/**
- * seed - this function clears the database, updates tables to
- *      match the models, and populates the database.
- */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
-  // Creating Users
-  const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+  let users = [];
+  let products = [];
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1]
-    }
+  // MAKE 100 FAKE USERS: We can change number of fake seeded users below, from 100.
+  for (let i = 0; i < 100; i++) {
+    const name = faker.name.firstName();
+
+    let newUser = {
+      // firstName: name,
+      // lastName: faker.name.lastName(),
+      username: faker.internet.userName(),
+      // kill this later
+      password: faker.internet.password(),
+      // email: faker.internet.email(name).toLowerCase(),
+      // addressLine1 // required, string
+      // addressLine2 // not required, string
+      // city // string
+      // zipCode // integer
+      // // CREDIT CARDS NOT REQUIRED
+      // creditCardNumber // integer
+      // creditCardName // string
+      // creditCardExpiration // string -- MM/YY format
+      // creditCardCVV //
+      // // Admin
+      // adminRights // boolean (seed one admin, everyoen else no, hardcode one admin, defaultValue false
+    };
+
+    users.push(newUser);
   }
+
+  // MAKE 100 FAKE PRODUCTS
+  // for (let i = 0; i < 100; i++) {
+  //   let newProduct = {
+  //     // what attributes?
+  //   };
+
+  //   products.push(newProduct);
+  // }
+
+  // For each user in the array, create a new user instance in the database
+  users.forEach(async (user) => {
+    await User.create(user);
+  });
+
+  console.log(`seeded ${users.length} users`);
+  // console.log(`seeded ${products.length} products`);
+  console.log(`seeded successfully`);
+  // The below seems unhelpful for large datasets.
+  // return {
+  //   users: {
+  //     cody: users[0],
+  //     murphy: users[1]
+  //   }
+  // }
 }
 
 /*
@@ -32,16 +74,18 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
-  } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.error(err);
+    process.exitCode = 1;
+    // MV: Troubleshooting "finally" below. Seed won't run with it,
+    // likely due to async.
+    // } finally {
+    //   console.log("closing db connection");
+    //   await db.close();
+    //   console.log("db connection closed");
   }
 }
 
@@ -51,8 +95,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
