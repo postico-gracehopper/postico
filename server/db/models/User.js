78cda2839b87deb22bec55808132f1b900490d42
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-10 18:15:22 -0500

    Updated JWT_SECRET .env call to match master

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -117,7 +117,7 @@ User.prototype.correctPassword = function (candidatePwd) {
 };
 
 User.prototype.generateToken = function () {
-  return jwt.sign({ id: this.id }, process.env.JWT);
+  return jwt.sign({ id: this.id }, process.env.JWT_SECRET);
 };
 
 /**
@@ -135,7 +135,7 @@ User.authenticate = async function ({ username, password }) {
 
 User.findByToken = async function (token) {
   try {
-    const { id } = await jwt.verify(token, process.env.JWT);
+    const { id } = await jwt.verify(token, process.env.JWT_SECRET);
     const user = User.findByPk(id);
     if (!user) {
       throw 'nooo';
