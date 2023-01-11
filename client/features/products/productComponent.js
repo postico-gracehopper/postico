c3d65ad653b11cc9857ed23d9e8175094961a73b
beq <blakebequette@gmail.com>
2023-01-11 17:01:14 -0600

    comment out product component for now

--- client/features/products/productComponent.js
+++ client/features/products/productComponent.js
@@ -4,33 +4,34 @@ import { fetchProductsAsync, selectProducts } from './productSlice';
 import { Link } from 'react-router-dom';
 
 const Products = () => {
-  const dispatch = useDispatch;
-  const products = useSelector(selectProducts);
+  return <div></div>}
+//   const dispatch = useDispatch;
+//   const products = useSelector(selectProducts);
 
-  useEffect(() => {
-    dispatch(fetchProductsAsync());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(fetchProductsAsync());
+//   }, [dispatch]);
 
-  return (
-    <div>
-      <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.id}>
-              <Link to={`/products/${product.id}`}>
-                <span>
-                  <h2>{product.name}</h2>
-                  <h2>{product.price}</h2>
-                </span>
-                <p>{product.description}</p>
-                <img src={product.imageUrl} />
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ul>
+//         {products.map((product) => {
+//           return (
+//             <li key={product.id}>
+//               <Link to={`/products/${product.id}`}>
+//                 <span>
+//                   <h2>{product.name}</h2>
+//                   <h2>{product.price}</h2>
+//                 </span>
+//                 <p>{product.description}</p>
+//                 <img src={product.imageUrl} />
+//               </Link>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </div>
+//   );
+// };
 
 export default Products;
