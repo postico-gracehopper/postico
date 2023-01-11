5a5751d7ef941c37b2939d525baaef4fc0df9719
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-11 12:31:00 -0500

    removed associations for deleted models

--- server/db/index.js
+++ server/db/index.js
@@ -4,26 +4,15 @@ const db = require('./db');
 
 const User = require('./models/User');
 const Product = require('./models/Product');
-const ShoppingCart = require('./models/ShoppingCart');
-const ShoppingCartItem = require('./models/ShoppingCartItem');
 const Order = require('./models/Order');
 const OrderItem = require('./models/OrderItem');
 
 //associations!
-User.hasOne(ShoppingCart);
-ShoppingCart.belongsTo(User);
-//
 User.hasMany(Order);
 Order.belongsTo(User);
 //
-Product.hasOne(ShoppingCartItem);
-ShoppingCartItem.belongsTo(Product);
-//
 Product.hasOne(OrderItem);
 OrderItem.belongsTo(Product);
-//
-ShoppingCart.hasMany(ShoppingCartItem);
-ShoppingCartItem.belongsTo(ShoppingCart);
 //
 Order.hasMany(OrderItem);
 OrderItem.belongsTo(Order);
@@ -34,8 +23,6 @@ module.exports = {
   models: {
     User,
     Product,
-    ShoppingCart,
-    ShoppingCartItem,
     Order,
     OrderItem,
   },
