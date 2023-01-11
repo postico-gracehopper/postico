4c0edb66e53a14f8d70c784b430318f1d83c6981
Bryan <irwinb14@gmail.com>
2023-01-11 12:09:31 -0800

    products slice and component done

--- server/db/db.js
+++ server/db/db.js
@@ -24,7 +24,7 @@ if (process.env.DATABASE_URL) {
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: { ssl: { rejectUnauthorized: false } },
-  logging: false
+  logging: false,
 });
 // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 
