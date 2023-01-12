985a455375629a8accda56b0748c94e6d5ab5706
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-12 13:22:28 -0500

    added a test route for checkout

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -5,6 +5,7 @@ import AuthForm from '../features/auth/AuthForm';
 import Home from '../features/home/Home';
 import { me } from './store';
 import Products from '../features/products/productComponent';
+import ShoppingCart from '../features/shoppingCart/ShoppingCart';
 
 /**
  * COMPONENT
@@ -25,6 +26,7 @@ const AppRoutes = () => {
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/checkout" element={<ShoppingCart />} />
         </Routes>
       ) : (
         <Routes>
