345fd21c3883d4f5c631d79270e34e00ce2e8687
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-12 13:22:38 -0500

    connected shoppingcart slice to store

--- client/app/store.js
+++ client/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/auth/authSlice';
 import productReducer from '../features/products/productSlice';
+import shoppingCartReducer from '../features/shoppingCart/shoppingCartSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
+    shoppingCart: shoppingCartReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
