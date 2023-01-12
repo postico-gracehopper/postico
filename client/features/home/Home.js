3ecea6bff718f52e9a06218cbb76017b5b014b41
Bryan <irwinb14@gmail.com>
2023-01-12 10:00:53 -0800

    single product view/add to cart button/home page shop button

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -10,6 +10,10 @@ const Home = (props) => {
   return (
     <div>
       <h3>Welcome, {username}</h3>
+      <img src="https://www.ischgl.com/media/ischgl/WINTER/SKIGEBIET/image-thumb__53475433__og-image/SKI_ALPIN_2022%20%287%29.webp" />
+      <Link to="/products">
+        <button type="button">Shop</button>
+      </Link>
     </div>
   );
 };
---
+++ client/features/products/singleProductSlice.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchSingleProductAsync = createAsyncThunk(
+  '/campus/fetchSingleProduct',
+  async (id) => {
+    try {
+      const { data } = await axios.get(`/api/products/${id}`);
+      return data;
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+const singleProductSlice = createSlice({
+  name: 'singleProduct',
+  initialState: {},
+  reucers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchSingleProductAsync.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
+});
+
+export default singleProductSlice.reducer;
+
+export const selectSingleProduct = (state) => state.singleProduct;
