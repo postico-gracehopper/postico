71f4c2953f2f5cf2fc8c2f5880b1e68f30b10cb3
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-12 09:24:35 -0500

    added fetch cart GET route

--- server/api/users.js
+++ server/api/users.js
@@ -84,6 +84,7 @@ router.get('/:id', verifyInteger, async (req, res, next) => {
 // })
 //
 
+// TODO: talk to blake about the above code and PUBLIC_USER_FIELDS
 //  fetch the order for a given user, including its associated orderItems
 router.get('/:id/cart', verifyInteger, async (req, res, next) => {
   try {
