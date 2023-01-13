2a032cfc4e5b7595e30a2d8e2d227afd6a1db829
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-13 10:31:17 -0600

    Login/sign up, script, NavBar elements for guests

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../app/store';
-// import logo from '../../../public/images/placeholder-logo.png';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -18,7 +17,6 @@ const Navbar = () => {
       <nav className="relative container mx-auto px-8 bg-[#f5f4f0]">
         {/* Flex container */}
         <div className="flex items-center justify-evenly">
-          {/* Logo - was previously image, using text for simplicity for now */}
           <div className="text-3xl font-bungee pl-1 pr-3 text-tahiti">
             Placeholder
           </div>
@@ -42,6 +40,12 @@ const Navbar = () => {
           ) : (
             <div className="hidden md:flex space-x-6 text-sm uppercase font-plex tracking-widest">
               {/* The navbar will show these links before you log in */}
+              <Link to="/home" className="hover:text-tahiti">
+                Home
+              </Link>
+              <Link to="/products" className="hover:text-tahiti">
+                Shop
+              </Link>
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
             </div>
