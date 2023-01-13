94c007fd51507ce15094f9747ca3249028b15895
beq <blakebequette@gmail.com>
2023-01-12 18:10:19 -0600

    make getUnwrapped cart - eager load user's cart

--- script/testOrder.js
+++ script/testOrder.js
@@ -3,9 +3,24 @@ const {
     models: { User, Product, Order, OrderItem  },
   } = require('../server/db');
 
-async function action() {
 
-    await Order.create({total: 11.23, orderPaid: false})
+
+async function action() {
+    const u = await User.findOne({where: {id: 78}}) //Math.floor(Math.random() * 99) + 1}})
+    //console.log(u)
+    // const p = await Product.findOne({where: {id: Math.floor(Math.random() * 100)}})
+    //console.log(p.id, p)
+    // const o = await Order.create({total: 34.32, orderPaid: false, userId: u.id})
+    // await OrderItem.create({
+    //     totalItemPrice: 13.12,
+    //     quantity: 2,
+    //     productId: p.id,
+    //     orderId: o.id,
+    // })
+    let cart = await u.getUnwrappedCart()
+    console.log(cart)
+    // return cart
+
 } 
 
 
--- server/db/index.js
+++ server/db/index.js
@@ -18,6 +18,84 @@ Order.hasMany(OrderItem);
 OrderItem.belongsTo(Order);
 //
 
+
+User.prototype.addToCart = async function(productId, qty){
+    const cart = await this.getCart()
+    const product = await Product.findOne({where: {id: productId}})
+    const [oItem, oItemWasCreated]= await OrderItem.findOrCreate({
+      where: {
+        orderId: cart.id,
+        productId: product.id
+      },
+      defaults: {
+        productId: product.id,
+        orderId: cart.id,
+      }})
+    await oItem.update({
+      quantity: (oItemWasCreated ? qty : oItem.quantity + qty),
+      totalItemPrice: (oItemWasCreated ? product.price 
+        : oItem.totalItemPrice + product.price*qty)
+    })
+}
+
+User.prototype.getCart = async function(){
+  const [cart, wasCreated] = await Order.findOne({
+    where: {
+      userId: this.id,
+      orderPaid: false
+    }})
+  return cart
+}
+
+User.prototype.getUnwrappedCart = async function(){
+  return User.getUnwrappedCartFor(this.id)
+}
+
+User.getUnwrappedCartFor = async function(usrId){
+  const cart = await Order.findOne({
+    where: {
+      userId: usrId,
+      orderPaid: false
+    },
+    include: {
+      model: OrderItem,
+      required: false,
+      
+      include: {
+        model: Product
+      },
+    },
+  })
+  return cart ? {       //UNPACKAGED CART
+    userId: usrId,
+    cartId: cart.id,
+    orderId: cart.id,
+    total: cart.total,
+    orderPaid: cart.orderPaid,
+    orderItems: cart.orderItems && cart.orderItems.length ? cart.orderItems.map(item => {
+      return {
+        productId: item.product.id,
+        name: item.product.name,
+        description: item.product.description,
+        price: item.product.price,
+        image: item.product.image,
+        category: item.product.category,
+        orderItemId: item.id,
+        quantity: item.quantity,
+        totalItemPrice: item.totalItemPrice,
+      }
+    }) : [] // if there is a cart, but no items
+  } : {
+    userId: usrId,
+    cartId: null,
+    orderId: null,
+    total: "00.00",        // DEFAULT CART
+    orderPaid: false,
+    orderItems: []
+  }
+} 
+
+
 module.exports = {
   db,
   models: {
