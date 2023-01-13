df76d2832e85f20403eabaae5096a69e47e9f03d
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-13 16:23:21 -0500

    cleanup

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,8 +1,5 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
-// const {
-//   models: { OrderItem },
-// } = require('../index');
 
 const Order = db.define('order', {
   total: {
@@ -14,31 +11,5 @@ const Order = db.define('order', {
     defaultValue: false,
   },
 });
-
-// instanceMethods
-
-// method to update total price... not sure if we can do this using the orderId in OrderItem that's yet to be associated...
-// TODO check calculations on cents
-// Order.prototype.updatePrice = async function () {
-//   const previousPrice = this.total;
-//   const orderItems = await OrderItem.findAll({ where: { orderId: this.id } });
-//   const newPrice = orderItems.reduce(
-//     (acc, item) => acc + item.totalItemPrice,
-//     0
-//   );
-//   this.total = newPrice;
-//   console.log(
-//     `Total order price updated from '${previousPrice}' to '${this.total}'`
-//   );
-// };
-
-/**
- * hooks
- */
-
-// Order.afterCreate(updatePrice());
-// Order.afterUpdate(updatePrice());
-// Order.afterSave(updatePrice());
-// Order.afterUpsert(updatePrice());
 
 module.exports = Order;
