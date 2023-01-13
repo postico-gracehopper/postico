6afc9afbd5df08599e46bff32fd6ffb5d490d898
Jamison Pereira <jamison.pereira@gmail.com>
2023-01-13 12:01:22 -0500

    added cart button

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -24,12 +24,14 @@ const Navbar = () => {
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
+            <Link to="/checkout">Checkout</Link>
           </div>
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <Link to="/checkout">Checkout</Link>
           </div>
         )}
       </nav>
