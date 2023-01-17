26f944bc98ea050802f2cbc7889cdcc29c481d6c
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-17 15:07:42 -0600

    Add cart icon (doesn't yet reflect cart total)

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '../../app/store';
+import CartIcon from './CartIcon';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
@@ -29,16 +30,17 @@ const Navbar = () => {
               <Link to="/products" className="hover:text-tahiti">
                 Shop
               </Link>
-              <Link to="/checkout" className="hover:text-tahiti">
-                Checkout
-              </Link>
-              <button
+              <span
                 type="button"
                 onClick={logoutAndRedirectHome}
-                className="hover:text-tahiti"
+                className="inline-block hover:text-tahiti"
               >
                 LOGOUT
-              </button>
+              </span>
+              <span>Tester</span>
+              <Link to="/checkout" className="hover:text-tahiti">
+                <CartIcon number="3" />
+              </Link>
             </div>
           ) : (
             <div className="hidden md:flex space-x-6 text-sm uppercase font-plex tracking-widest">
@@ -49,11 +51,11 @@ const Navbar = () => {
               <Link to="/products" className="hover:text-tahiti">
                 Shop
               </Link>
-              <Link to="/checkout" className="hover:text-tahiti">
-                Checkout
-              </Link>
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
+              <Link to="/checkout" className="hover:text-tahiti">
+                <CartIcon number="1" />
+              </Link>
             </div>
           )}
         </div>
