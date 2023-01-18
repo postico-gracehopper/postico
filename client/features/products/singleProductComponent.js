4eaa87a95b75488232212e0f172343e73291166a
Meridith Viguet <meridith.viguet@gmail.com>
2023-01-18 15:05:09 -0600

    Refined pdp view

--- client/features/products/singleProductComponent.js
+++ client/features/products/singleProductComponent.js
@@ -18,42 +18,30 @@ const SingleProduct = () => {
 
   return (
     <div>
-      <section className="text-gray-700 body-font overflow-hidden bg-white">
+      <section className="text-stone overflow-hidden">
         <div className="container px-5 py-24 mx-auto">
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img
-              className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+              className="lg:w-1/2 w-full object-cover object-center rounded drop-shadow-lg"
               src={product.image}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+              <h2 className="text-xs tracking-widest uppercase">
                 {product.category}
               </h2>
-              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                {product.title}
+              <h1 className="text-2xl font-plex uppercase tracking-widest font-medium mb-4">
+                {product.name}
               </h1>
-              <p className="leading-relaxed">{product.description}</p>
-              <span className="absolute right-0 top-0 h-full w-10 text-center text-stone pointer-events-none flex items-center justify-center">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
-              <div className="flex">
+              <p className="leading-relaxed text-xs">{product.description}</p>
+              <hr className="h-px my-8 bg-stone border-0"></hr>
+              <div className="flex justify-between">
                 <span className="font-plex text-2xl text-stone">
-                  ${product.price} //
+                  ${product.price}
                 </span>
                 <AddToCartButton
                   product={product}
                   quantity={1}
-                  className="text-2xl"
+                  className="text-xl "
                 />
               </div>
             </div>
