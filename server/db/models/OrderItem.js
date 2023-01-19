9a53b9f274513eb66231731055212b071ae3d99d
Jamison Pereira <113544207+jamisonpereira@users.noreply.github.com>
2023-01-19 09:25:41 -0500

    added >= 0 validation for item quantity

--- server/db/models/OrderItem.js
+++ server/db/models/OrderItem.js
@@ -7,6 +7,9 @@ const OrderItem = db.define('orderItem', {
   quantity: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
+    validate: {
+      min: 0,
+    },
   },
   totalItemPrice: {
     type: Sequelize.DECIMAL,
